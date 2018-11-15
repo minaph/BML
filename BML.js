5a383d1b8baf8b7793f708282fa7d1f299287019
Yuki Minoh <43029787+minaph@users.noreply.github.com>
2018-11-15 10:22:27 +0900

    improved readability

--- BML.js
+++ BML.js
@@ -1,1 +1,14 @@
-javascript: var google = "'https://www.google.co.jp/search?q='"; var bmt = "'" + prompt("ブックマークのテーマを入力") + "'"; var kw = ""; var kw_t = ""; var win = ", '_blank', 'width=500,height=1000');"; var tab = ", '_blank');"; do { kw_t = kw; kw += "+' " + prompt("デフォルトキーワードを入力") + "'"; } while (kw != kw_t + "+' '"); var ans = "javascript:window.open(" + google + "+ prompt(" + bmt + ") " + kw; if(confirm("検索結果を新規ウィンドウで開きますか？")){ans+=win;}else{ans+=tab;} alert(ans);
+javascript:
+var google = "'https://www.google.co.jp/search?q='";
+var bmt = "'" + prompt("ブックマークのテーマを入力") + "'";
+var kw = "";
+var kw_t = "";
+var win = ", '_blank', 'width=500,height=1000');";
+var tab = ", '_blank');";
+do {
+    kw_t = kw;
+    kw += "+' " + prompt("デフォルトキーワードを入力") + "'";
+} while (kw != kw_t + "+' '");
+var ans = "javascript:window.open(" + google + "+ prompt(" + bmt + ") " + kw;
+if(confirm("検索結果を新規ウィンドウで開きますか？")){ans+=win;}else{ans+=tab;}
+alert(ans);
